bf0db5c42233261df33b7cd446860d1601f0c979
nuilewis <lewisnuikweh@gmail.com>
2022-12-07 21:01:21 +0100

    Added interner connection checker

--- lib/repositories/devotional_repository.dart
+++ lib/repositories/devotional_repository.dart
@@ -2,6 +2,7 @@ import 'package:christabodeadmin/services/devotional/devotional_firestore_servic
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dartz/dartz.dart';
 
+import '../core/connection_checker/connection_checker.dart';
 import '../core/errors/failure.dart';
 import '../models/devotional_model.dart';
 
@@ -17,34 +18,43 @@ abstract class DevotionalRepository {
 
 class DevotionalRepositoryImplementation implements DevotionalRepository {
   final DevotionalFirestoreService devotionalFirestoreService;
+  final ConnectionChecker connectionChecker;
 
   DevotionalRepositoryImplementation(
-      {required this.devotionalFirestoreService});
+      {required this.devotionalFirestoreService,
+      required this.connectionChecker});
 
   ///Todo: maybe add conenction checker
 
-  List<Devotional> _devotionalList = [];
+  final List<Devotional> _devotionalList = [];
   final String currentYear = DateTime.now().year.toString();
 
   @override
   Future<Either<Failure, List<Devotional>>> getDevotionals(String? year) async {
-    try {
-      QuerySnapshot querySnapshot =
-          await devotionalFirestoreService.getDevotionals();
+    if (await connectionChecker.isConnected) {
+      try {
+        QuerySnapshot querySnapshot =
+            await devotionalFirestoreService.getDevotionals();
 
-      if (querySnapshot.docs.isNotEmpty) {
-        for (DocumentSnapshot element in querySnapshot.docs) {
-          Map<String, dynamic> documentData =
-              element.data() as Map<String, dynamic>;
+        if (querySnapshot.docs.isNotEmpty) {
+          for (DocumentSnapshot element in querySnapshot.docs) {
+            Map<String, dynamic> documentData =
+                element.data() as Map<String, dynamic>;
 
-          Devotional devotional =
-              Devotional.fromMap(data: documentData, docId: element.id);
-          _devotionalList.add(devotional);
+            Devotional devotional =
+                Devotional.fromMap(data: documentData, docId: element.id);
+            _devotionalList.add(devotional);
+          }
         }
+        return Right(_devotionalList);
+      } on FirebaseException catch (e) {
+        return Left(FirebaseFailure(errorMessage: e.message, code: e.code));
+      } catch (e) {
+        return const Left(
+            FirebaseFailure(errorMessage: "An unknown error has occured"));
       }
-      return Right(_devotionalList);
-    } on FirebaseException catch (e) {
-      return Left(FirebaseFailure(errorMessage: e.message, code: e.code));
+    } else {
+      return const Left(NetworkFailure());
     }
   }
 
@@ -67,32 +77,40 @@ class DevotionalRepositoryImplementation implements DevotionalRepository {
   @override
   Future<Either<Failure, void>> editDevotionalMessage(
       {required Devotional devotional, String? year}) async {
-    try {
-      await devotionalFirestoreService.editDevotionalMessage(
-          devotional: devotional, year: year ?? currentYear);
-      return Right(Future.value());
-    } on FirebaseException catch (e) {
-      return Left(FirebaseFailure(errorMessage: e.message, code: e.code));
-    } catch (e) {
-      return const Left(FirebaseFailure(
-          errorMessage:
-              "An unknown error occurred while trying to edit this message"));
+    if (await connectionChecker.isConnected) {
+      try {
+        await devotionalFirestoreService.editDevotionalMessage(
+            devotional: devotional, year: year ?? currentYear);
+        return Right(Future.value());
+      } on FirebaseException catch (e) {
+        return Left(FirebaseFailure(errorMessage: e.message, code: e.code));
+      } catch (e) {
+        return const Left(FirebaseFailure(
+            errorMessage:
+                "An unknown error occurred while trying to edit this message"));
+      }
+    } else {
+      return const Left(NetworkFailure());
     }
   }
 
   @override
   Future<Either<Failure, void>> uploadDevotionalMessage(
       {required Devotional devotional, String? year}) async {
-    try {
-      await devotionalFirestoreService.addDevotionalMessage(
-          devotional: devotional, year: year ?? currentYear);
-      return Right(Future.value());
-    } on FirebaseException catch (e) {
-      return Left(FirebaseFailure(errorMessage: e.message, code: e.code));
-    } catch (e) {
-      return const Left(FirebaseFailure(
-          errorMessage:
-              "An unknown error occurred while trying to upload this message"));
+    if (await connectionChecker.isConnected) {
+      try {
+        await devotionalFirestoreService.addDevotionalMessage(
+            devotional: devotional, year: year ?? currentYear);
+        return Right(Future.value());
+      } on FirebaseException catch (e) {
+        return Left(FirebaseFailure(errorMessage: e.message, code: e.code));
+      } catch (e) {
+        return const Left(FirebaseFailure(
+            errorMessage:
+                "An unknown error occurred while trying to upload this message"));
+      }
+    } else {
+      return const Left(NetworkFailure());
     }
   }
 }
